6169e5071233d543e944f32443db04d4421fefab
hundredvisionsguy <winikkc@hsd.k12.or.us>
2016-05-26 09:41:41 -0700

    Added html5-reset-child.js

--- wp-content/themes/HTML5-Reset-Child/functions.php
+++ wp-content/themes/HTML5-Reset-Child/functions.php
@@ -20,6 +20,7 @@ function theme_enqueue_styles() {
 		get_stylesheet_directory_uri() . '/css/bootstrap-theme.min.css'
 	);
 	wp_enqueue_script( 'bootstrap.min', get_stylesheet_directory_uri() . '/js/bootstrap.min.js', '3.0.0', true);
+	wp_enqueue_script( 'html5-reset-child', get_stylesheet_directory_uri() . '/js/html5-reset-child.js');
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
